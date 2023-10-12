0915655fc28621dbee4e4a0f4b937312e807e594
brorae <jhr3239@gmail.com>
2023-10-12 17:51:36 +0900

    chore: init project

---
+++ shilly-shally/src/main/java/com/shillyshally/ShillyShallyApplication.java
@@ -0,0 +1,13 @@
+package com.shillyshally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShillyShallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShillyShallyApplication.class, args);
+	}
+
+}
---
+++ shilly-shally/src/test/java/com/shillyshally/ShillyShallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shillyshally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShillyShallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
