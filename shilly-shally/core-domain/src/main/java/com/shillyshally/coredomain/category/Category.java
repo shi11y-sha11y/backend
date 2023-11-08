3a6fd31f7b55908220c16383f80bcc13dfc83d0f
brorae <jhr3239@gmail.com>
2023-11-09 01:38:23 +0900

    feat: Category 리스트 조회 api 구현

--- shilly-shally/core-domain/src/main/java/com/shillyshally/coredomain/category/Category.java
+++ shilly-shally/core-domain/src/main/java/com/shillyshally/coredomain/category/Category.java
@@ -6,8 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
