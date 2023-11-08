68e707b04a7320128ada09852e5560f61ded5f01
brorae <jhr3239@gmail.com>
2023-11-09 01:00:45 +0900

    feat: Category entity 추가

--- shilly-shally/core-domain/src/main/java/com/shillyshally/coredomain/category/Category.java
+++ shilly-shally/core-domain/src/main/java/com/shillyshally/coredomain/category/Category.java
@@ -1,41 +1,21 @@
-package com.shillyshally.coredomain.restaurant;
+package com.shillyshally.coredomain.category;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
-public class Restaurant {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-
-    private Long categoryId;
-
-    private String categoryDetail;
-
-    private String phone;
-
-    private String url;
-
-    private String imageUrl;
-
-    private Double longitude;
-
-    private Double latitude;
 }
--- shilly-shally/core-domain/src/main/java/com/shillyshally/coredomain/restaurant/Category.java
+++ shilly-shally/core-domain/src/main/java/com/shillyshally/coredomain/restaurant/Category.java
@@ -11,7 +11,13 @@ public enum Category {
     KOREAN("한식"),
     CHINESE("중식"),
     JAPANESE("일식"),
-    WESTERN("양식");
+    WESTERN("양식"),
+    ASIAN("아시안"),
+    SNACK_BAR("분식"),
+    MEAT("고기"),
+    SANDWICH_SALAD("샌드위치/샐러드"),
+    CAFE("카페"),
+    BAR("술집");
 
     private final String label;
 
