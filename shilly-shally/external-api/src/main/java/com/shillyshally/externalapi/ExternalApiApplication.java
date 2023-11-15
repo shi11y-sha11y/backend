2d2587b56e1fe17adfa2073afd9837a0a6325f19
brorae <jhr3239@gmail.com>
2023-11-15 23:08:17 +0900

    docs: ControllerTest를 통한 카테고리 API 문서 작성

--- shilly-shally/external-api/src/main/java/com/shillyshally/externalapi/ExternalApiApplication.java
+++ shilly-shally/external-api/src/main/java/com/shillyshally/externalapi/ExternalApiApplication.java
@@ -2,12 +2,8 @@ package com.shillyshally.externalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.shillyshally.coredomain"})
-@EnableJpaRepositories(basePackages = {"com.shillyshally.coredomain"})
 public class ExternalApiApplication {
 
     public static void main(String[] args) {
